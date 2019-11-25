d60a95a98a601cae5fe3183331669f9e9a69a590
yagobrandalise <56320958+yagobrandalise@users.noreply.github.com>
2019-11-24 22:40:10 -0300

    Add new classes

--- User.cs
+++ User.cs
@@ -17,4 +17,34 @@ namespace register.core
         public int Month { get; set; }
         public int Day { get; set; }
     }
+    
+     public class Student 
+    {
+        public Subject[] Classes { get; set; }
+        public string[] ParentsId { get; set; }
+        public int Registration { get; set; }
+        public Subject[] History { get; set; }
+    }
+
+    public class Subject
+    {
+        public string Id { get; set; }
+        public Student[] Students { get; set; }
+        public string Professor { get; set; }
+        public DateTime Schedule { get; set; }
+        public int ClassId { get; set; }
+        public string Tasks { get; set; }
+        public string Local { get; set; }
+        public float Value { get; set; }
+    }
+
+    public class Professor
+    {
+        public Subject[] Subjects { get; set; }
+    }
+
+    public class Parents
+    {
+        public string SonId { get; set; }
+    }
 }
